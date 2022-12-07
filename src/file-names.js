902dbbca5f1ae2bc03b2f0277d8ff0b71ba23c4e
Maria Gornashevich <lightbrown9874@gmail.com>
2022-12-07 19:31:47 +0300

    feat: add file-names and get-email

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  for (let i = 0; i < names.length; i++) {
+    let count = 0;
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        count++;
+        names[j] = `${names[i]}(${count})`;
+      }
+    }
+  }
+  return names;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
