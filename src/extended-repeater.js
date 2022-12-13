95553c8f600b3534fa88b5695ecfbb02272209e5
Maria Gornashevich <lightbrown9874@gmail.com>
2022-12-13 03:53:53 +0300

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,17 +15,42 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/*str, options*/) {
-  // let targetStr = "";
+function repeater(str, options) {
+  let targetStr = "";
 
-  // const repeatTimes = "repeatTimes" in options ? options.repeatTimes : 1;
-  // const separator = "separator" in options ? options.separator : "+";
-  // const addition = "addition" in options ? options.addition : "";
-  // const additionRepeatTimes =
-  //   "additionRepeatTimes" in options ? options.additionRepeatTimes : 1;
-  // const additionSeparator =
-  //   "additionSeparator" in options ? options.additionSeparator : "|";
-  throw new NotImplementedError('Not implemented');
+  const repeatTimes = "repeatTimes" in options ? options.repeatTimes : 1;
+  const separator = "separator" in options ? options.separator : "+";
+  const addition = "addition" in options ? options.addition : "";
+  const additionRepeatTimes =
+    "additionRepeatTimes" in options ? options.additionRepeatTimes : 1;
+  const additionSeparator =
+    "additionSeparator" in options ? options.additionSeparator : "|";
+
+  const arr = [];
+  arr.push(
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  );
+
+  for (let a = 0; a < arr[0]; a++) {
+    targetStr = targetStr + str;
+    const repeatLength = arr[0] - 1;
+    const additionLength = arr[3] - 1;
+
+    for (let b = 0; b < arr[3]; b++) {
+      if (b < additionLength) {
+        targetStr = targetStr + arr[2] + arr[4];
+      } else {
+        targetStr = targetStr + arr[2];
+      }
+    }
+    a < repeatLength ? (targetStr += arr[1]) : (targetStr += "");
+  }
+
+  return targetStr;
 }
 
 module.exports = {
